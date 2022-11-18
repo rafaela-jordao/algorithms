617f77df272009b7ba5a5358553f426ce84e8ad0
Rafaela Jordão <rafa.jordao.07@hotmail.com>
2022-11-18 16:27:13 -0300

    feat: implementa algoritmo de busca (número de estudantes estudando no mesmo horário)

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,5 +1,20 @@
-# iniciando o projeto
+# permanence_period = lista de tuplas onde é armazenado os dados de entrada e saída de estudantes.
+# cada tupla representa o período de permanência de uma pessoa estudante no sistema com seu horário de entrada e de saída.
+# Nos arrays temos 6 estudantes:
+# estudante             1       2       3       4       5       6
+# permanence_period = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
+
+# Req. 1 - Número de estudantes estudando no mesmo horário (Algoritmo de busca)
 
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    count = 0
+
+    for index in permanence_period:
+        try:
+            if index[0] <= target_time <= index[1]:
+                count += 1
+        except TypeError:
+            return None
+
+    return count
+
