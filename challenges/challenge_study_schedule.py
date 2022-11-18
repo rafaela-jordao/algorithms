ecb0e94a6c34668f16f9a3dc65fa6f6237c3d076
Rafaela Jordão <rafa.jordao.07@hotmail.com>
2022-11-18 16:47:40 -0300

    feat: corrigi linter

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,11 +1,3 @@
-# permanence_period = lista de tuplas onde é armazenado os dados de entrada e saída de estudantes.
-# cada tupla representa o período de permanência de uma pessoa estudante no sistema com seu horário de entrada e de saída.
-# Nos arrays temos 6 estudantes:
-# estudante             1       2       3       4       5       6
-# permanence_period = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
-
-# Req. 1 - Número de estudantes estudando no mesmo horário (Algoritmo de busca)
-
 def study_schedule(permanence_period, target_time):
     count = 0
 
@@ -17,4 +9,3 @@ def study_schedule(permanence_period, target_time):
             return None
 
     return count
-
